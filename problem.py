6f4d318cd50d0abd37bf654b19d4b561f0fbb461
Cesar Solano <171cas@gmail.com>
2022-04-22 09:59:13 -0500

    getting really close

--- problem.py
+++ problem.py
@@ -18,21 +18,69 @@ map_dij = {
 
 
 def find(map, start, end):
-    mat = {}
-    visited = set()
+    matrix = {}
+    unvDic = {}
     unvisited = set(key for key in map.keys())
+
     for key in unvisited:
-        mat[key] = [INF,None]
+        matrix[key] = [INF, None]
+        unvDic[key] = INF
 
-    current_city = start
-    unvisited.remove(start)
-    mat[start]=[0,None]
+    # current_city = start
+    # unvisited.remove(start)
+    matrix[start] = [0, None]
+    unvDic[start] = 0
+    # test = {'a':1,'b':123, 'asdf':45, 'asdfff':-50}
 
-    # while unvisited:
-    #     for city in map[current_city]:
+    # unvisited.remove(current_city)
+    # current_city = min(test, key=test.get)
+    i=0
+    while i<3:
+        current_city= min(unvDic, key=unvDic.get)
+        print('current_city',current_city)
+        print('unvDic',unvDic)
+        print('matrix',matrix)
+        print('--------------------------------------------------------')
+        neighbors = [city[0] for city in list(map[current_city])]
+        # print('current_city',current_city)
+        # print('map[current_city]',map[current_city])
+        # print('list(map[current_city])',list(map[current_city]))
 
 
+        for city in list(map[current_city]):
+            # print('city',city)
+            # print('matrix[city[0]]',matrix[city[0]])
+            # print('2--------------------------------------------------------')
+            if city[1] < matrix[city[0]][0]:
+                matrix[city[0]][0]= city[1]
+                matrix[city[0]][1]= current_city
+                unvDic[city[0]] = city[1]
 
-    return mat
 
-print(find(map_dij,'Seattle','Miami'))
+        # print('matrix',matrix)
+        unvisited.remove(current_city)
+        del unvDic[current_city]
+        i=i+1
+
+
+    return matrix
+
+print(find(map_dij, 'Seattle', 'Miami'))
+
+# print(min(d, key=d.get))
+# for city in map[current_city]:
+#             print(city[0])
+#         current_city = unvisited.pop()
+
+
+    # for pair in map[current_city]:
+    #      neighbors[pair[0]] = pair[1]
+
+
+    # print(unvDic)
+    # print(unvisited)
+    # print(matrix)
+
+
+    # visited = set()
+    # neighbors = {}
